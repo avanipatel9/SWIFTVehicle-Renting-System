4f515e0a4892ce468f6d5272d8b14223772c14fa
Avani Patel <anpatel36@yahoo.in>
2020-02-10 21:01:33 -0500

    Initial Commit

---
+++ SWIFTVehicle Renting System/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SWIFTVehicle Renting System
+//
+//  Created by Avani Patel on 2/10/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
