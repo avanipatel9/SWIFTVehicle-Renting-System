a218c170012a588357be9db8f8308ffa2eee9cfd
Avani Patel <anpatel36@yahoo.in>
2020-02-11 23:51:19 -0500

    Created Car class

--- SWIFTVehicle Renting System/Car.swift
+++ SWIFTVehicle Renting System/Car.swift
@@ -1,12 +1,14 @@
 //
-//  main.swift
+//  Car.swift
 //  SWIFTVehicle Renting System
 //
-//  Created by Avani Patel on 2/10/20.
+//  Created by Avani Patel on 2/11/20.
 //  Copyright © 2020 Avani Patel. All rights reserved.
 //
 
 import Foundation
 
-
-
+class Car: Vehicle
+{
+    
+}
