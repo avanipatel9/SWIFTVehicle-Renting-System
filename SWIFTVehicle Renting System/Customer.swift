504aada6db68fd6f6ac6398605ffda42f8ec9b04
Avani Patel <anpatel36@yahoo.in>
2020-02-12 20:59:09 -0500

    [AP] created  Customer class and attributes are added in that

---
+++ SWIFTVehicle Renting System/Customer.swift
@@ -0,0 +1,15 @@
+//
+//  Customer.swift
+//  SWIFTVehicle Renting System
+//
+//  Created by Avani Patel on 2/12/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+class Customer: Person {
+    var address: String
+    var city: String
+    var vehicleRent
+    
+}
