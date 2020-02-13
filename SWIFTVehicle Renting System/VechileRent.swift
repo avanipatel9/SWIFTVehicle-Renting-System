e952635a4dd080289a862146eae70c5975dbc64d
CharmiPatel25 <c0768448@mylambton.ca>
2020-02-12 21:12:24 -0500

    [CP]Adding structure Vehicle Rent

---
+++ SWIFTVehicle Renting System/VechileRent.swift
@@ -0,0 +1,18 @@
+//
+//  VechileRent.swift
+//  SWIFTVehicle Renting System
+//
+//  Created by MacStudent on 2020-02-12.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+struct VechileRent
+{
+    var startDate:Date
+    var endDate:Date
+    var noOfDays:Int
+    var noOfKmDrived:Float
+    var totalBillToPay:Double
+   
+}
