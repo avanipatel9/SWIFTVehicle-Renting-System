f9646a0a3b77d8bad243742eb8f77d3602ff1289
Shreya1503 <shreyavaghasia99@gmail.com>
2020-02-12 20:37:57 -0500

    Creating MotorCycle Class

---
+++ SWIFTVehicle Renting System/MotorCycle.swift
@@ -0,0 +1,14 @@
+//
+//  MotorCycle.swift
+//  SWIFTVehicle Renting System
+//
+//  Created by MacStudent on 2020-02-12.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+
+class MotorCycle : Vehicle
+{
+    
+}
