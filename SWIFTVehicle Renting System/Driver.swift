08d5a1c0b3543aeb527301db6fa45bfe7327dd66
Shreya1503 <shreyavaghasia99@gmail.com>
2020-02-12 20:42:49 -0500

    Creating Driver Class

---
+++ SWIFTVehicle Renting System/Driver.swift
@@ -0,0 +1,16 @@
+//
+//  Driver.swift
+//  SWIFTVehicle Renting System
+//
+//  Created by MacStudent on 2020-02-12.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+
+class Driver
+{
+    var drivingLicenceNumber : String
+    var isHistroyCleared : Bool
+    var salary : Double
+}
