6fe5230461d82608329f5d9d5f22b29820117b14
Avani Patel <anpatel36@yahoo.in>
2020-02-19 17:16:35 -0500

    [AP] Initializer added in MotorCycle class

--- SWIFTVehicle Renting System/MotorCycle.swift
+++ SWIFTVehicle Renting System/MotorCycle.swift
@@ -10,6 +10,12 @@ import Foundation
 
 class MotorCycle : Vehicle
 {
-    var maxTopSpeed : Int?
-    var milage : String?
+    var maxTopSpeed : Int
+    var milage : Double
+    
+    init(vehicleIdentificationNumber: Int, vehicleDescription: String, manufacturerName: String, isInsured: Bool, insuranceProviderName: String, numberOfSeat: Int, fuelType: FuelType, maxTopSpeed: Int, milage: Double) {
+        self.maxTopSpeed=maxTopSpeed
+        self.milage=milage
+        super.init(vehicleIdentificationNumber: vehicleIdentificationNumber, vehicleDescription: vehicleDescription, manufacturerName: manufacturerName, isInsured: isInsured, insuranceProviderName: insuranceProviderName, numberOfSeat: numberOfSeat, fuelType: fuelType)
+    }
 }
