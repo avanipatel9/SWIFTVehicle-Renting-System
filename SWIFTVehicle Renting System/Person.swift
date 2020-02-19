4f270a79fb15430df95c6b7bdda8b013fea83476
charmi patel <c0768448@mylambton.ca>
2020-02-19 15:23:07 -0500

    [CP] adding init for person class

--- SWIFTVehicle Renting System/Person.swift
+++ SWIFTVehicle Renting System/Person.swift
@@ -15,12 +15,26 @@ enum Gender
 
 class Person
 {
-    var firstName : String?
-    var lastName : String?
-    var gender : Gender?
-    var mobileNumber : String?
-    var birthDate : Date?
-    var emailID : String?
-    var userName : String?
-    var password : String?
+    var firstName : String
+    var lastName : String
+    var gender : Gender
+    var mobileNumber : String
+    var birthDate : Date
+    var emailID : String
+    var userName : String
+    var password : String
+    var age:Int
+    
+    init(firstName:String,lastName:String,gender:Gender,mobileNumber:String,birthDate:Date,emailID:String,userName:String,password:String)
+    {
+        self.firstName=firstName
+        self.lastName=lastName
+        self.gender=gender
+        self.mobileNumber=mobileNumber
+        self.emailID=emailID
+        self.userName=userName
+        self.password=password
+        self.birthDate=birthDate
+        self.age=Calendar.current.dateComponents([.year], from: birthDate,to: Date()).year!
+    }
 }
