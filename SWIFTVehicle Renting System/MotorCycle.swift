c733bf18454da8512c381f3f27e508a94c7ce1d0
charmi patel <c0768448@mylambton.ca>
2020-02-19 18:41:43 -0500

    [CP]Conforming motorcycle class to vehicle protocol

--- SWIFTVehicle Renting System/MotorCycle.swift
+++ SWIFTVehicle Renting System/MotorCycle.swift
@@ -10,12 +10,51 @@ import Foundation
 
 class MotorCycle : Vehicle
 {
+    var vehicleIdentificationNumber: Int
+    
+    var vehicleDescription: String
+    
+    var manufacturerName: String
+    
+    var isSelfDrive: Bool
+    
+    var driver: String
+    
+    var isInsured: Bool
+    
+    var insuranceProviderName: String
+    
+    var numberOfSeat: Int
+    
+    var fuelType: FuelType
+    
+    var isVehicleRented: Bool
+    
+    func Display() {
+         print("dd")
+    }
+    
+    func DisplayAvailableVehicleToRent() {
+         print("dd")
+    }
+    
+   
+    
     var maxTopSpeed : Int
     var milage : Double
     
-    init(vehicleIdentificationNumber: Int, vehicleDescription: String, manufacturerName: String, isInsured: Bool, insuranceProviderName: String, numberOfSeat: Int, fuelType: FuelType, maxTopSpeed: Int, milage: Double) {
+    init(vehicleIdentificationNumber: Int, vehicleDescription: String, manufacturerName: String,isSelfDrive: Bool,driver: String, isInsured: Bool, insuranceProviderName: String, numberOfSeat: Int, fuelType: FuelType,isVehicleRented: Bool, maxTopSpeed: Int, milage: Double) {
         self.maxTopSpeed=maxTopSpeed
         self.milage=milage
-        super.init(vehicleIdentificationNumber: vehicleIdentificationNumber, vehicleDescription: vehicleDescription, manufacturerName: manufacturerName, isInsured: isInsured, insuranceProviderName: insuranceProviderName, numberOfSeat: numberOfSeat, fuelType: fuelType)
+       self.vehicleIdentificationNumber=vehicleIdentificationNumber
+           self.vehicleDescription=vehicleDescription
+           self.manufacturerName=manufacturerName
+       self.isSelfDrive=isSelfDrive
+       self.driver=driver
+           self.isInsured=isInsured
+           self.insuranceProviderName=insuranceProviderName
+           self.numberOfSeat=numberOfSeat
+           self.fuelType=fuelType
+       self.isVehicleRented=isVehicleRented
     }
 }
