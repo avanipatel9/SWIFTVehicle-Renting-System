c0ab937652ba40221142eee592d292baa1bd1089
charmi patel <c0768448@mylambton.ca>
2020-02-19 15:35:07 -0500

    [CP] Adding attributes to owner class

--- SWIFTVehicle Renting System/Owner.swift
+++ SWIFTVehicle Renting System/Owner.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+class Owner:Person
+{
+    var companyTitle:String
+    var landlineNumber:String
+    var website:String
+    var vehicleListOwned:[String:Vehicle]?
+    
+    init(companyTitle:String,landlineNumber:String,website:String,firstName:String,lastName:String,gender:Gender,mobileNumber:String,birthDate:Date,emailID:String,userName:String,password:String) {
+        self.companyTitle=companyTitle
+        self.landlineNumber=landlineNumber
+        self.website=website
+        super.init(firstName: firstName, lastName: lastName, gender: gender, mobileNumber: mobileNumber, birthDate: birthDate, emailID: emailID, userName: userName, password: password)
+    }
+    
+}
+
