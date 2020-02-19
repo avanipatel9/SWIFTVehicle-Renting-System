7a3dfd03552432a244a7b5d462b930d7d1a6e385
charmi patel <c0768448@mylambton.ca>
2020-02-19 18:44:38 -0500

    [CP] Conforming Bus class to vehicle protocol

--- SWIFTVehicle Renting System/Bus.swift
+++ SWIFTVehicle Renting System/Bus.swift
@@ -9,15 +9,55 @@
 import Foundation
 class Bus:Vehicle
 {
+    var vehicleIdentificationNumber: Int
+    
+    var vehicleDescription: String
+    
+    var manufacturerName: String
+    
+    var isSelfDrive: Bool
+    
+    var driver: String
+    
+    var isInsured: Bool
+    
+    var insuranceProviderName: String
+    
+    var numberOfSeat: Int
+    
+    var fuelType: FuelType
+    
+    var isVehicleRented: Bool
+    
+    func Display() {
+         print("dd")
+    }
+    
+    func DisplayAvailableVehicleToRent() {
+         print("dd")
+    }
+    
+    
+    
     var busType:String
     var isServiceAvailable:Bool
     var   isWifiAvailable:Bool
     
     //Initializer
-    init(busType:String,isServiceAvailable:Bool,isWIfiAvailable:Bool)
+    init(vehicleIdentificationNumber: Int, vehicleDescription: String, manufacturerName: String,isSelfDrive: Bool,driver: String, isInsured: Bool, insuranceProviderName: String, numberOfSeat: Int, fuelType: FuelType,isVehicleRented: Bool,busType:String,isServiceAvailable:Bool,isWIfiAvailable:Bool)
     {
         self.busType=busType
         self.isWifiAvailable=isWIfiAvailable
         self.isServiceAvailable=isServiceAvailable
+        self.vehicleIdentificationNumber=vehicleIdentificationNumber
+            self.vehicleDescription=vehicleDescription
+            self.manufacturerName=manufacturerName
+        self.isSelfDrive=isSelfDrive
+        self.driver=driver
+            self.isInsured=isInsured
+            self.insuranceProviderName=insuranceProviderName
+            self.numberOfSeat=numberOfSeat
+            self.fuelType=fuelType
+        self.isVehicleRented=isVehicleRented
     }
 }
