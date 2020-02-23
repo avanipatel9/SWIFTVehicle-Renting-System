462fb11f532056a273c65eb7827238401e6a2c5a
Avani Patel <anpatel36@yahoo.in>
2020-02-23 01:51:20 -0500

    [AP] function added to create customer in singleton class

--- SWIFTVehicle Renting System/main.swift
+++ SWIFTVehicle Renting System/main.swift
@@ -94,10 +94,6 @@ dr.addVehicle(vehicle: b5)
 
 //Create vehicleRent object
 let vr1 = VehicleRent(startDate: Date.from(year: 2019, month: 07, day: 27)!, endDate: Date.from(year: 2019, month: 07, day: 30)!, vechile: car1, noOfKmDrived: 50.0)
-if vr1.vehicle.isSelfDrive==false
-{
-    vr1.vehicle.driver=vr1.vehicle.setDriver(driver: d1)
-}
 let vr2 = VehicleRent(startDate: Date.from(year: 2020, month: 02, day: 21)!, endDate: Date.from(year: 2020, month: 02, day: 23)!, vechile: b1, noOfKmDrived: 500.0)
 let vr3 = VehicleRent(startDate: Date.from(year: 2018, month: 04, day: 01)!, endDate: Date.from(year: 2018, month: 04, day: 05)!, vechile: b2, noOfKmDrived: 1500.0)
 let vr4 = VehicleRent(startDate: Date.from(year: 2019, month: 05, day: 27)!, endDate: Date.from(year: 2019, month: 05, day: 28)!, vechile: car2, noOfKmDrived: 200.0)
@@ -169,40 +165,8 @@ do
 
 
 //Create Customers
-do{
-    let c1 = try Customer(id: "C001", firstName: "Cristen", lastName: "Little", gender: .Female, mobileNumber: "6471202489", birthDate: Date.from(year: 1993, month: 01, day: 15)!, emailID: "cristen.little@gmail.com", userName: "Cristen123", password: "pasword", address: "18 Young Street", city: "Toronto")
-    c1.addVehicleListRented(vehicleRented: vr1, vehicleIdentificationNumber:vr1.vehicle.vehicleIdentificationNumber )
-    c1.addVehicleListRented(vehicleRented: vr2, vehicleIdentificationNumber: vr2.vehicle.vehicleIdentificationNumber)
-    c1.addVehicleListRented(vehicleRented: vr3, vehicleIdentificationNumber: vr3.vehicle.vehicleIdentificationNumber)
-    c1.addVehicleListRented(vehicleRented: vr4, vehicleIdentificationNumber: vr4.vehicle.vehicleIdentificationNumber)
-    c1.addVehicleListRented(vehicleRented: vr5, vehicleIdentificationNumber: vr5.vehicle.vehicleIdentificationNumber)
-    c1.addVehicleListRented(vehicleRented: vr6, vehicleIdentificationNumber: vr6.vehicle.vehicleIdentificationNumber)
-    dr.addCustomer(customer: c1)
-    c1.Display()
-}
-do{
-    
-    let c2 = try Customer(id: "C002", firstName: "Wes", lastName: "Dundon", gender: .Male, mobileNumber: "458369127", birthDate: Date.from(year: 2005, month: 02, day: 20)!, emailID: "wes@gmail.com", userName: "wes45", password: "wes@12345", address: "55 Huntario Street", city: "Brampton")
-    c2.addVehicleListRented(vehicleRented: vr10, vehicleIdentificationNumber: vr10.vehicle.vehicleIdentificationNumber)
-    c2.Display()
-}
+dr.createCustomers()
 
-do{
-    let c3 = try Customer(id: "C003", firstName: "Merrick", lastName: "Dondon", gender: .Male, mobileNumber: "6478952146", birthDate: Date.from(year: 1997, month: 05, day: 24)!, emailID: "merrick@gmail.com", userName: "merricl45", password: "merrickD", address: "8,Derry Road", city: "Toronto")
-    c3.addVehicleListRented(vehicleRented: vr8, vehicleIdentificationNumber: vr8.vehicle.vehicleIdentificationNumber)
-    c3.addVehicleListRented(vehicleRented: vr9, vehicleIdentificationNumber: vr9.vehicle.vehicleIdentificationNumber)
-    c3.addVehicleListRented(vehicleRented: vr7, vehicleIdentificationNumber: vr7.vehicle.vehicleIdentificationNumber)
-    c3.Display()
-}
 
-do{
-    let c4 = try Customer(id: "C004", firstName: "Samuel", lastName: "Nichol", gender: .Male, mobileNumber: "587963147", birthDate: Date.from(year: 1996, month: 08, day: 08)!, emailID: "samuel@gmail.com", userName: "Samuel_09", password: "samuel09", address: "798,Westwood", city: "Missisagua")
-    c4.addVehicleListRented(vehicleRented: vr11, vehicleIdentificationNumber: vr11.vehicle.vehicleIdentificationNumber)
-    c4.Display()
-}
-do{
-    let c5 =  try Customer(id: "C005", firstName: "Valaria", lastName: "Fitzer", gender: .Female, mobileNumber: "6472589634", birthDate: Date.from(year: 1993, month: 12, day: 24)!, emailID: "valaria@gmail.com", userName: "Valaria_@34", password: "valaria89", address: "303,Darcel Avenue", city: "Oakville")
-    c5.Display()
-}
-
+dr.displayAllCustomers()
 dr.displayAvailableVehicleToRent()
