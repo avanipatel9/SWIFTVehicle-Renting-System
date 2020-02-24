c20b4740f9cc301d05db62b3e04d5b8b01e0a3cb
chyarmi <c0768448@mylambton.ca>
2020-02-24 17:25:13 -0500

    [CP]  Create custom exception class

---
+++ SWIFTVehicle Renting System/Utility/CustomException.swift
@@ -0,0 +1,9 @@
+//
+//  CustomException.swift
+//  SWIFTVehicle Renting System
+//
+//  Created by MacStudent on 2020-02-24.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
