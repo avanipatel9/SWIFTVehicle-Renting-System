04513b70a470f4e85e41d31d68ef17e668b0af45
chyarmi <c0768448@mylambton.ca>
2020-02-24 17:57:30 -0500

    [CP] Add logic for error handling in owner class

--- SWIFTVehicle Renting System/Person/Owner.swift
+++ SWIFTVehicle Renting System/Person/Owner.swift
@@ -21,7 +21,7 @@ class Owner:Person//, Codable
     
     var birthDate: Date?
     
-    var emailID: String
+    var emailID: String?
     
     var userName: String
     
@@ -42,20 +42,37 @@ class Owner:Person//, Codable
     
     
     
-    init(id:String, firstName: String, lastName: String, gender: Gender, mobileNumber: String, birthDate: Date, emailID: String, userName: String, password: String, companyTitle:String, landlineNumber:String, website:String)
+    init(id:String, firstName: String, lastName: String, gender: Gender, mobileNumber: String, birthDate: Date, emailID: String, userName: String, password: String, companyTitle:String, landlineNumber:String, website:String) throws
     {
         self.id = id
         self.firstName = firstName
         self.lastName = lastName
         self.gender = gender
+        if mobileNumber.count != 10
+        {
+          print("Invalid Mobile Number \(mobileNumber) : Cannot be less than 10 digits")
+            throw  CustomException.invalidNumber
+            
+        }
         self.mobileNumber = mobileNumber
         self.birthDate = birthDate
-        self.emailID = emailID
+        //self.emailID = emailID
         self.userName = userName
         self.password = password
         self.companyTitle=companyTitle
         self.landlineNumber=landlineNumber
         self.website=website
+        if isEmailValid(email: emailID)
+                  {
+                     
+                      self.emailID=emailID
+                  }
+                   else
+                   {
+                       print("Invalid Email ID for \(self.firstName) : \(emailID) ")
+                       throw CustomException.invalidEmail
+               
+                   }
         
     }
     
@@ -65,6 +82,13 @@ class Owner:Person//, Codable
         
     }
     
+    func isEmailValid(email:String)  -> Bool {
+           let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+           
+           let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+           return emailTest.evaluate(with: email)
+       }
+       
     
     func Display() {
         print("First Name : \(self.firstName)")
